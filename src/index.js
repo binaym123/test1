88bdac67b9c02653634a276a91ace79f2c6db391
binaym <wikiwoo@binayms-MacBook-Pro.local>
2023-09-27 22:54:56 +0300

    Add some code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 
 import './index.css';
 import App from './App';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-ReactDOM.render(
-  <BrowserRouter>
+const root = createRoot(document.getElementById('root'));
+
+root.render(
     <App />
-  </BrowserRouter>,
-  // document.getElementById('root')
-  document.getElementById('root')
 );
